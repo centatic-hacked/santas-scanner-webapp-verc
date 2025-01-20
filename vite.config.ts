c7362aa9b9f37aad552b6cee2047ebc75a5ba9a6
Alexander Hack <113599884+centatic-hacked@users.noreply.github.com>
2025-01-20 20:33:21 +0100

    Vite Upd

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
   server: {
     port: (process.env.PORT || 3000) as number,
-  }
+    host: true,
+    strictPort: true,
+  },
 });
